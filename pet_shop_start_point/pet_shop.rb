55fcc63fe20a77b2ce4f325e667210d8ea523638
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-04-15 19:08:23 +0100

    add remove cash

--- pet_shop_start_point/pet_shop.rb
+++ pet_shop_start_point/pet_shop.rb
@@ -5,3 +5,11 @@ end
 def total_cash(pet_shop)
   return pet_shop[:admin][:total_cash]
 end
+
+def add_or_remove_cash(pet_shop, amount) 
+  if amount.is_a? Integer
+    pet_shop[:admin][:total_cash]  += amount
+  else
+    return nil
+  end
+end
